1a7a69839165e01313273f94a81ee12de10da48e
John Jastrow <jjastrow>
2016-04-03 17:32:52 -0500

    lecture updates

--- src/ToDo.java
+++ src/ToDo.java
@@ -15,36 +15,45 @@ public class ToDo {
             System.out.println("1. Create to-do item");
             System.out.println("2. Toggle to-do item");
             System.out.println("3. List to-do items");
+            System.out.println("Q. Quit");
 
-            String option = scanner.nextLine();
+            String option = scanner.nextLine().toUpperCase();
 
-            if (option.equals("1")) {
-                System.out.println("Enter your to-do item:");
-                String text = scanner.nextLine();
+            switch (option) {
+                case "1" : {
+                    System.out.println("Enter your to-do item:");
+                    String text = scanner.nextLine();
 
-                ToDoItem item = new ToDoItem(text, false);
-                items.add(item);
-            }
-            else if (option.equals("2")) {
-                System.out.println("Enter the number of the item you want to toggle:");
-                int itemNum = Integer.valueOf(scanner.nextLine());
-                ToDoItem item = items.get(itemNum-1);
-                item.isDone = !item.isDone;
-            }
-            else if (option.equals("3")) {
-                int i = 1;
-                for (ToDoItem item : items) {
-                    String checkbox = "[ ] ";
-                    if (item.isDone) {
-                        checkbox = "[x] ";
-                    }
-                    System.out.println(checkbox + i + ". " + item.text);
-                    i++;
+                    ToDoItem item = new ToDoItem(text, false);
+                    items.add(item);
+                    break;
                 }
+                case "2" : {
+                    System.out.println("Enter the number of the item you want to toggle:");
+                    int itemNum = Integer.valueOf(scanner.nextLine());
+                    ToDoItem item = items.get(itemNum-1);
+                    item.isDone = !item.isDone;
+                    break;
+                }
+                case "3" : {
+                    displayList(items);
+                    break;
+                }
+                case "Q": return;
+                default: System.out.println("Invalid option");
             }
-            else {
-                System.out.println("Invalid option");
+        }
+    }
+
+    private static void displayList(ArrayList<ToDoItem> items){
+        int i = 1;
+        for (ToDoItem item : items) {
+            String checkbox = "[ ] ";
+            if (item.isDone) {
+                checkbox = "[x] ";
             }
+            System.out.println(String.format("%s%s. %s", checkbox, String.valueOf(i), item.text));
+            i++;
         }
     }
 }
